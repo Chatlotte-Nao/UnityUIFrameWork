16c5944d00b55e93a0940faf98ccee81c43bf6ab
Chatlotte-Nao <651061950@qq.com>
2024-12-16 20:02:32 +0800

    清空仓库

--- Assets/ZMUIFrameWork/Scripts/Editor/GeneratorConfig.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GeneratorConfig
-{
-    public static string FindComponentGeneratorPath = Application.dataPath + "ZMUIFrameWork/Scripts/FindComponent";
-}
