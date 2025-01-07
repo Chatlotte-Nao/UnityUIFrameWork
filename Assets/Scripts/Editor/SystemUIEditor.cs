06d5089188c9c4358341d2fb54a6e1a4521462cc
you <651061950@qq.com>
2025-01-07 11:50:36 +0800

    解决拖拽到场景时未赋值UICamera问题

--- Assets/Scripts/Editor/SystemUIEditor.cs
+++ Assets/Scripts/Editor/SystemUIEditor.cs
@@ -6,14 +6,15 @@ using UnityEngine.UI;
 
 public class SystemUIEditor : Editor
 {
-    //Allow an editor class method to be initialized when Unity loads without action from the user.
-    //InitializeOnLoadMethod特性允许unity在运行时调用这个方法
+    //InitializeOnLoadMethod这个特性
+    // 所标记的方法会在 Unity 编辑器加载时（比如打开项目、脚本重新编译、进入或退出 Play 模式）被自动调用。
+    // 无需手动调用，也无需挂载到特定对象。
     [InitializeOnLoadMethod]
     private static void InitEditor()
     {
         //监听hierarchy发生改变的委托
         EditorApplication.hierarchyChanged += HandleTextOrImageRaycast;
-        EditorApplication.hierarchyChanged += HandleTextOrImageRaycast;
+        EditorApplication.hierarchyChanged += LoadWindowCamera;
     }
 
     private static void HandleTextOrImageRaycast()
@@ -42,6 +43,21 @@ public class SystemUIEditor : Editor
 
     private static void LoadWindowCamera()
     {
-        
+        if (Selection.activeGameObject != null)
+        {
+            GameObject uiCameraObj = GameObject.Find("UICamera");
+            if (uiCameraObj != null)
+            {
+                Camera camera = uiCameraObj.GetComponent<Camera>();
+                if (Selection.activeGameObject.name.Contains("Window"))
+                {
+                    Canvas canvas = Selection.activeGameObject.GetComponent<Canvas>();
+                    if (canvas != null)
+                    {
+                        canvas.worldCamera = camera;
+                    }
+                }
+            }
+        }
     }
 }
